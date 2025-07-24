916c56b55ec1550dd992bf997dd6314ce584013d
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-07-24 05:49:51 +0000

    comet done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const account_Id = 635622
+let accountEmail = "palsudhir3213@gmail.com"
+var accountPassword = "Sudhir@22432"
+accountCity = "Surat"
+let accountstate;
+
+// account_Id = 52 // not allowed
+ accountEmail ="3533@gmail.com"
+ accountPassword = "54444"
+accountCity = "jaipur"
+
+console.log(account_Id);
+console.table ([ account_Id,accountEmail,accountPassword,accountCity,accountstate])
