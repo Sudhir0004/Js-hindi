6c072970aae7aa178682a998ca75ed1b1badeca6
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-07-29 08:20:55 +0000

    done

--- .devcontainer/01_basics/01_basic/03_conversionoperation.js
+++ .devcontainer/01_basics/01_basic/03_conversionoperation.js
@@ -12,8 +12,37 @@ let score = undefined  //suppose
  let someNumber= "66"
 let stringnumber=(someNumber);
 
-console.log( typeof stringnumber);
+//console.log( typeof stringnumber);
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 //console.log(valueInNumber);
+
+//******************operation *//
+let value= 3
+let negvalue= -value
+//console.log(negvalue);
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**2);
+ //console.log(2/2);
+ //console.log(2%2);
+  let str1= "hello"
+   let str2= "hero"
+
+   let str3= str1+str2
+  // console.log(str3);
+
+  // console.log(1+"5");
+   //console.log("1"+5+5); 
+      //console.log("1"+5+"5");  not using in companies overated /unused fooled people used 
+  // console.log(1+5+"5");  
+  let boolean= (true)
+//   console.log(boolean);
+ let num1,num2,num3
+ num1=num2=num3=2+2
+ let gamecounter = 110
+ gamecounter++;
+ ++gamecounter;
+ console.log(gamecounter)
