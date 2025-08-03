d2804af45132329fec313c4b6b2c2617b9585be4
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-08-03 15:40:28 +0000

    data type done

---
+++ .devcontainer/01_basics/01_basic/05_datatype.js
@@ -0,0 +1,35 @@
+//Primitive Data 
+
+//7 types= STRINGS,NUMBER,BOOLEAN,NULL=EMPTY NOT "0",UNDEFINED = NOT GIVEN ANY VALUE , SYMBOL,BIGINT
+  
+
+const score = 88 
+const scorevalue= 55 
+const isloggedin = false
+
+const isloggedIn= false 
+const outsideTemp = null
+
+ let userEmail;
+
+ const id = Symbol('123')
+const anotherId = Symbol('123')
+ 
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+// console.log(id === anotherid);
+//REFERENCE(NON PRIMITVE)
+//array,objects,function
+
+
+const heros =["shaktiman,bheem"]
+let myobJ = {
+   name:"sudhir",
+   age: 18,
+   
+}
+ const myFunction= function(){
+    console.log("Hello world");
+ }
+ console.log( typeof outsideTemp);
