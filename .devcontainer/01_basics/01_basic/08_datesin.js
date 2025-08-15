e33ad2257b7f90f3fb08589988f4c06aa6b8876c
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-08-15 05:47:57 +0000

    datejsdone

--- .devcontainer/01_basics/01_basic/08_datesin.js
+++ .devcontainer/01_basics/01_basic/08_datesin.js
@@ -1,4 +1,33 @@
 //  dates
  let  myDate = new Date ()
- console.log(myDate);
+//  console.log(myDate);
+//  console.log(myDate.toString());
+//  console.log(myDate.toLocaleString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toISOString());
+//  console.log(typeof myDate);
+
+//   let mycreateddate = new Date("2025,8,15")
+//   let mycreateddate = new Date(2025-8-15,5,3)
+//   let mycreateddate = new Date("2025-8-15")
+  let mycreateddate = new Date("2025-8-15")
+//  console.log(mycreateddate.toDateString());
+//  console.log(mycreateddate.toLocaleString());
+ let mytimestamp=Date.now()
+
+//  console.log(mytimestamp);
+//  console.log(mycreateddate.getTime());
+// //  to cnovert in second
+//  console.log(Math.floor(Date.now()/1000));
  
+let newDate = new Date()
+
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+// '${newDate.getDate()} and the time
+ newDate.toLocaleString('default',{
+    Weekday:"Long",
+ })
