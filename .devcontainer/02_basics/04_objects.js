08cb85ebaacb569a6abb87126a381fb7dff68273
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-08-19 14:36:50 +0000

    d

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,52 @@
+// const tinderuser =new Object()  //this singleton object
+const tinderuser = {}  //this is not singleton object
+tinderuser.id= "sudhir004",
+tinderuser.name = "sudhir" ,
+tinderuser.isloggedin = false,
+
+
+console.log(tinderuser);
+//aanother aap
+
+const1  = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+
+// console.log( regularuser.fullname.userfullname.firstname);
+
+// const obj1={1: "a",2: "b"}
+// const obj2={3: "a",4: "b"}
+// const obj4={5: "a",6: "b"}
+// const obj3={obj1,obj2}
+// const obj3= Object.assign({},obj1,obj2,obj4)
+// const obj3={...obj1, ...obj2, ...obj4}
+//  console.log(obj3);
+
+const user = [
+ {
+    id: 1,
+    email: "sudhir@dcj"
+ },
+  {
+    id: 2,
+    email: "sudhir@dcj"
+ },
+  
+ {
+    id: 3,
+    email: "sudhir@dcj"
+ }
+]
+ user[1].email
+ console.log(Object.keys(tinderuser));
+ console.log(Objec.values(tinderuser));
+ console.log(Object.entries(tinderuser));
+
+ 
