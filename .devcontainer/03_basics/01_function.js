7f1143c8341596e81d349f11f1b17c254ac97106
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-08-26 15:27:11 +0000

    okk

---
+++ .devcontainer/03_basics/01_function.js
@@ -0,0 +1,32 @@
+function saymyname(){
+console.log(S);
+console.log(U);
+console.log(D);
+console.log(H);
+console.log(I);
+console.log(R);
+
+}
+// saymyname()
+// function addTwonumbers(number1,number2){
+//     console.log(number1+number2);
+    
+function addTwonumbers(number1,number2){
+    // let result = number1+number2
+    // console.log("sudhir");
+   return number1+number2
+   return result
+// console.log("sudhir");>>>>
+}  //after result anything will not print >>>>>
+ const result=  addTwonumbers(3,"5");
+//  console.log("result:" ,result);
+
+function loginuserMessage(username="sam"){
+    if (username== undefined) {
+    console.log("Please enter a username");
+     return
+    }
+    return'${username} just looged in'
+}
+// console.log(loginuserMessage("sudhir"))
+console.log(loginuserMessage("sudhir"));
