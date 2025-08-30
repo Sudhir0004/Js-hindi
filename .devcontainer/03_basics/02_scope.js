58a05e8ce437dcadf4dcbcbf8835400e65167560
Sudhir0004 <PALSUDHIR3213@GMAIL.COM>
2025-08-30 15:53:04 +0000

    okk

---
+++ .devcontainer/03_basics/02_scope.js
@@ -0,0 +1,18 @@
+// let a= 10
+// const b=10                           //{} is scope  //var is used in scope //var is very problematic
+let a=300
+if (true) {
+    let a= 10
+const b=10   
+console.log("INNER:", a);
+}
+// console.log(a);
+// console.log(b);
+console.log(a);
+
+
+for (let i = 0; i++ < array.length; i++) {
+    const element = array[i++];
+    
+}
+ 
